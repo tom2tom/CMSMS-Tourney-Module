50b2879ba85c735e0ad8bb654a3f846e6175aab0
tom2tom <tpgww@onepost.net>
2015-02-07 13:14:40 +1100

    wider days,hours columns

--- method.uninstall.php
+++ method.uninstall.php
@@ -6,22 +6,57 @@ Refer to licence and other details at the top of file Tourney.module.php
 More info at http://dev.cmsmadesimple.org/projects/tourney
 */
 
+//NB caller must be very careful that top-level dir is valid!
+function delTree($dir)
+{
+	$files = array_diff(scandir($dir),array('.','..'));
+	if($files)
+	{
+		foreach($files as $file)
+		{
+			$fp = cms_join_path($dir,$file);
+			if(is_dir($fp))
+			{
+			 	if(!delTree($fp))
+					return false;
+			}
+			else
+				unlink($fp);
+		}
+		unset($files);
+	}
+	return rmdir($dir);
+}
+
 if (! $this->CheckAccess('admin')) return;
 
 $pref = cms_db_prefix();
 $db = cmsms()->GetDb();
 // uploaded files
-$config = cmsms()->GetConfig();
-$updir = $config['uploads_path'];
-if($updir) //careful, we're going to delete stuff wildly
+
+$fp = $config['uploads_path'];
+if($fp && is_dir($fp))
 {
-	$rel = $this->GetPreference('uploads_dir',$this->GetName());
-	if ($rel)
+	$ud = $this->GetPreference('uploads_dir','');
+	if($ud)
 	{
-		$updir .= DIRECTORY_SEPARATOR.$rel;
-		$mask = $updir.DIRECTORY_SEPARATOR.'*';
-		array_map("unlink",glob($mask,GLOB_NOSORT)); //assumes no populated subdir
-		unlink($updir);
+		$fp = cms_join_path($fp,$ud);
+		if($fp && is_dir($fp))
+			delTree($fp);
+	}
+	else
+	{
+		$files = $db->GetCol("SELECT DISTINCT chartcss FROM ".$pref."module_tmt_brackets
+WHERE chartcss IS NOT NULL AND chartcss<>''");
+		if($files)
+		{
+			foreach($files as $fn)
+			{
+				$fn = cms_join_path($fp,$fn);
+				if(is_file($fn))
+					unlink($fn);
+			}
+		}
 	}
 }
 
@@ -71,3 +106,4 @@ $this->RemoveEventHandler('Core','LoginPost');
 $this->Audit(0, $this->Lang('friendlyname'), $this->Lang('uninstalled'));
 
 ?>
+
