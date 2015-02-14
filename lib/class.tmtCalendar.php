53ce7295f2bff2c9829d93901a268a6fc6d56d27
tom2tom <tpgww@onepost.net>
2015-02-14 16:29:02 +1100

    first version of condition-parsing

--- lib/class.tmtCalendar.php
+++ lib/class.tmtCalendar.php
@@ -7,7 +7,10 @@ More info at http://dev.cmsmadesimple.org/projects/tourney
 
 Class: tmtCalendar.
 */
-class tmtCalendar {
+class tmtCalendar
+{
+	private $dayblocks;
+	private $timeblocks;
 
 	/**
 	CheckCondition:
@@ -36,22 +39,181 @@ class tmtCalendar {
 				//in case the user mistakenly added more dots ....
 				while($one[1][0] == '.')
 					$one[1] = substr($one[1],1);
+				//TODO ensure [0] is before [1]
 			}
 		}
 		unset($one);
 		return $parts;
 	}
 
-	//assumes no whitespace in $str
+	/**
+	_ParseDayCondition:
+	$str: the string to be parsed
+
+	Populate self::dayblocks with days-range array(s), each value therein an
+	integer relative to start of current year
+
+	Assumes no whitespace in $str.
+	*/
 	private function _ParseDayCondition($str)
 	{
 		$parts = self::_SplitCondition($str);
 	}
 
-	//assumes no whitespace in $str
-	private function _ParseHourCondition($str)
+	//Sort array in which any member(s) may be a range-array. All strings, caseless.
+	private static function _cmp_blocks($a,$b)
+	{
+		if(is_array($a))
+		{
+			if(is_array($b))
+				return strcmp($a[0],$b[0]);
+			elseif($a[0] === $b)
+				return 1; //range-arrays last
+			return strcmp($a[0],$b);
+		}
+		elseif(is_array($b))
+		{
+			if($a === $b[0])
+				return -1; //range-arrays last
+			return strcmp($a,$b[0]);
+		}
+		else
+			return strcmp($a,$b);
+	}
+
+/*
+	$sunstuff = FALSE, or
+	array(
+		'rise'=>translated 'sunrise' string
+		'set'=>translated 'sunset' string
+		'lat'=>latitude for calc
+		'long'=>longitude for calc
+		'zone'=>name of timezone (for calc GMT offset)
+		'day'=>timestamp for day being processed
+	)
+*/
+	private function _CleanTime($str,$sunstuff)
+	{
+		if($str[0] == '-')
+		{
+			preg_match('/(2[0-3]|[0-1]?[0-9])(:[0-5]?[0-9])?/',$str,$matches,0,1);
+			if($matches[0])
+			{
+				$adj = 24 - $matches[0];
+				while ($adj < 0)
+					$adj += 24;
+				if($matches[1])
+					$adj .= $matches[1];
+				return $adj;
+			}
+			return substr($str,1); //TODO better error handling
+		}
+		if($sunstuff)
+		{
+			if(strpos($str,$sunstuff['rise']) === 0)
+			{
+					$offs = 0; //TODO calc timezone offset func($sunstuff['zone']) +/- custom offset
+					return date_sunrise($sunstuff['day'],SUNFUNCS_RET_STRING,
+						$sunstuff['lat'],$sunstuff['long'],90+(50/60),$offs);
+			}
+			if(strpos($str,$sunstuff['rise']) === 0)
+			{
+					$offs = 0; //TODO calc timezone offset func($sunstuff['zone']) +/- custom offset
+					return date_sunrise($sunstuff['day'],SUNFUNCS_RET_STRING,
+						$sunstuff['lat'],$sunstuff['long'],90+(50/60),$offs);
+			}
+		}
+		return $str; //TODO better error handling
+	}
+
+	/**
+	_ParseHourCondition:
+	$str: the string to be parsed
+	$sunstuff: optional array of sunrise/set calculation parameters as below, default FALSE
+	array(
+		'rise'=>translated 'sunrise' string as would appear in module parameter value
+		'set'=>translated 'sunset' string as would appear in module parameter value
+		'lat'=>latitude for calc
+		'long'=>longitude for calc
+		'zone'=>name of timezone (for calc GMT offset)
+		'day'=>timestamp for day being processed
+	)
+
+	Populate self::timeblocks with seconds-range array(s), each value therein a
+	timestamp relative to start of current year
+
+	Assumes no whitespace in $str.
+	*/
+	private function _ParseHourCondition($str,$sunstuff=FALSE)
 	{
 		$parts = self::_SplitCondition($str);
+		//cleanup
+		foreach($parts as &$one)
+		{
+			if(is_array($one))
+			{
+				$s = $one[0];
+				$f = $s[0];
+				if($f < '0' || $f > '9')
+					$s = self::_CleanTime($s,$sunstuff);
+				$e = $one[1];
+				$f = $e[0];
+				if($f < '0' || $f > '9')
+					$e = self::_CleanTime($e,$sunstuff);
+				if(strcmp($s,$e) < 0)
+					$one = array($s,$e);
+				elseif(strcmp($s,$e) > 0)
+					$one = array($e,$s);
+				else
+					$one = $s;
+			}
+			else
+			{
+				$f = $one[0];
+				if($f < '0' || $f > '9')
+					$one = self::_CleanTime($one,$sunstuff);
+			}
+		}
+		unset($one);
+		if($count($parts) > 1)
+			usort($parts, array('tmtCalendar','_cmp_blocks'));
+		//coalesce
+//	$tvals = array(0,0,0,1,1,date("Y")); //start of current year (CHECKME or 1970?)
+//	$base = mktime($tvals);
+		$base = mktime(0,0,0,1,1);
+		$blocks = array(array(-1,-1)); //always have something to compare
+		foreach($parts as &$one)
+		{
+			if(is_array($one))
+			{
+/*			$tvals[0] = func($one[0]); //hour
+				$tvals[1] = func($one[0]); //minute
+				$stamp = mktime($tvals) - $base;
+				$tvals[0] = func($one[1]);
+				$tvals[1] = func($one[1]);
+				$stend = mktime($tvals) - 1 - $base;
+*/
+				$stamp = strtotime($one[0],$base); //no need for time localisation?
+				$stend = strtotime($one[1],$base);
+			}
+			else
+			{
+/*			$tvals[0] = func($one); //hour
+				$tvals[1] = func($one); //minute
+				$stamp = mktime($tvals) - $base;
+*/
+				$stamp = strtotime($one,$base);
+				$stend = $stamp + 3599;
+			}
+			$blend = end($blocks);
+			if ($stamp > $blend[1] + 1)
+				$blocks[] = array($stamp,$stend);
+			elseif($stend > $blend[1])
+				$blend[1] = $stend;
+		}
+		unset($one);
+		array_shift($blocks);
+		$this->timeblocks = $blocks;
 	}
 
 	/**
@@ -69,7 +231,28 @@ class tmtCalendar {
 	{
 		if($daycond == FALSE && $hourcond == FALSE)
 			return TRUE;
-		//TODO
+		if($daycond)
+		{
+			//TODO [re]init $this->dayblocks?
+			self::_ParseDayCondition($daycond);
+		}
+		elseif ($this->dayblocks)
+		{
+			unset($this->dayblocks);
+			$this->dayblocks = FALSE;
+		}
+		if($hourcond)
+		{
+			//TODO [re]init $this->timeblocks?
+			$sunstuff = FALSE; //TODO
+			self::_ParseHourCondition($hourcond,$sunstuff);
+		}
+		elseif ($this->timeblocks)
+		{
+			unset($this->timeblocks);
+			$this->timeblocks = FALSE;
+		}
+		//TODO find whether start..start+length fits within dayblocks + timeblocks
 		return FALSE;
 	}
 
@@ -91,7 +274,10 @@ class tmtCalendar {
 	{
 		if($daycond == FALSE && $hourcond == FALSE)
 			return $start;
-		//TODO
+		self::_ParseDayCondition($daycond);
+		$sunstuff = FALSE; //TODO
+		self::_ParseHourCondition($hourcond,$sunstuff);
+		//TODO find in dayblocks + timeblocks next interval matching start..start+length 
 		return 0;
 	}
 
