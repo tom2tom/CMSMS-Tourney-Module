19ef559c725cd0991b1ca7dad86f86a19e743089
tom2tom <tpgww@onepost.net>
2015-02-17 07:33:17 +1100

    extra calendar help

--- lang/en_US.php
+++ lang/en_US.php
@@ -404,7 +404,9 @@ Any P or T can be<br />
 &#8226; a bracket-enclosed and comma-separated sequence of values (in any order)<br />
 &#8226; a '..' separated range of sequential values<br />
 For dates, month and day, or just day, are optional. Times are 24-hour, minutes are optional, 
-the minute-separator must be ':'. Non-ranged times each represent one hour. Other numeric values may be < 0, meaning count backwards.<br />
+the minute-separator must be ':'. In some contexts, expressing a time like H:00 may
+be needed to discriminate between hours-of-day and days-of-month. Non-ranged times
+each represent one hour. Other numeric values may be < 0, meaning count backwards.<br />
 Examples:<br />
 &#8226; 2000 or 2000..2005 or 2000-6 or 2000-10..2001-3 or 2000-9-1 or 2000-10-1..2000-12-31<br />
 &#8226; January or November..December<br />
