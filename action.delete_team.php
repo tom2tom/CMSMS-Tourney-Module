0c988e1a8aff5574d48c52d29c45b8784948f5c4
tom2tom <tpgww@onepost.net>
2015-02-21 17:58:44 +1100

    cleanup batchwise re-order teams

--- action.delete_team.php
+++ action.delete_team.php
@@ -7,18 +7,6 @@ More info at http://dev.cmsmadesimple.org/projects/tourney
 
 Mark single team, or selected team(s), as deleted (flags = 2)
 */
-if(!function_exists('OrderTeamsBatch'))
-{
- function OrderTeamsBatch(&$db,$pref,$teams,$o)
- {
-	$sql = 'UPDATE '.$pref.'module_tmt_teams SET displayorder = CASE team_id ';
-	foreach($teams as $tid)
-		$sql .= 'WHEN '.(int)$tid.' THEN '.($o++).' ';
-	$sql .= 'ELSE displayorder END';
-	$db->Execute($sql);
- }
-}
-
 $newparms = $this->GetEditParms($params,'playerstab');
 if ($this->CheckAccess('admod'))
 {
@@ -40,17 +28,22 @@ if ($this->CheckAccess('admod'))
 	$sql = 'UPDATE '.$pref.'module_tmt_teams SET flags=2 WHERE team_id'.$cond;
 	$db->Execute($sql,$args);
 	//update remaining displayorders
+	//TODO find better approach for this, without db-specific stuff
 	$sql = 'SELECT team_id FROM '.$pref.'module_tmt_teams WHERE bracket_id=? AND flags!=2 ORDER BY displayorder';
 	$teams = $db->GetCol($sql,array($params['bracket_id']));
-	if ($teams)
+	if($teams)
 	{
-		$adbg = array();
+		$tl = count($teams) - 1; //for 0-based compares
 		$o = 0;
-		$tl = count($teams) - 1;
 		do
 		{
-			$batch = array_slice($teams,$o,8,TRUE);
-			OrderTeamsBatch($db,$pref,$batch,$o+1);
+			$batch = array_slice($teams,$o,8,TRUE); //no need to re-index
+			$vo = $o+1; //1-based order
+			$sql = 'UPDATE '.$pref.'module_tmt_teams SET displayorder = CASE team_id ';
+			foreach($batch as $tid)
+				$sql .= 'WHEN '.$tid.' THEN '.($vo++).' ';
+			$sql .= 'ELSE displayorder END WHERE bracket_id=?';
+			$db->Execute($sql,array($params['bracket_id']));
 			$o += 8;
 		} while($o < $tl);
 	}
