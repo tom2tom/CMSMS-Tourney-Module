a5cadee770ef3ccb68326439d945b55e5001f418
tom2tom <tpgww@onepost.net>
2015-02-26 19:17:12 +1100

    import funcs from parent class

--- lib/class.tmtCalendar.php
+++ lib/class.tmtCalendar.php
@@ -16,40 +16,30 @@ class tmtCalendar extends Calendar
 	}
 	
 	//No checks here for valid parameters - assumed done before
+	//$start is bracket-local timestamp
 	private function _GetSunData(&$bdata,$start)
 	{
-		$cond = $this->cleaned;
-		if($cond)
+		if($bdata['timezone'])
+			$zone = $bdata['timezone'];
+		else
 		{
-			$rise = 'R'; //alias for $this->mod->Lang('sunrise');
-			$set = 'S'; //alias for $this->mod->Lang('sunset');
-			if(strpos($cond,$rise) !== FALSE || strpos($cond,$set) !== FALSE)
-			{
-				if($bdata['timezone'])
-					$zone = $bdata['timezone'];
-				else
-				{
-					$zone = $this->mod->GetPreference('time_zone','');
-					if(!zone)
-						$zone = 'Europe/London'; //TODO BETTER e.g. func($bdata['locale'])
-				}
-				$lat = $bdata['latitude']; //maybe 0.0
-				$long = $bdata['longitude']; //ditto
-				$daystamp = floor($start/84600)*84600; //midnight
-				return array (
-				 'rise'=>$rise,
-				 'set'=>$set,
-				 'day'=>$daystamp,
-				 'lat'=>$lat,
-				 'long'=>$long,
-				 'zone'=>$zone
-				);
-			}
+			$zone = $this->mod->GetPreference('time_zone','');
+			if(!zone)
+				$zone = 'Europe/London'; //TODO BETTER e.g. func($bdata['locale'])
 		}
-		return FALSE;
+		$lat = $bdata['latitude']; //maybe 0.0
+		$long = $bdata['longitude']; //ditto
+		$daystamp = floor($start/84600)*84600; //midnight
+		return array (
+		 'day'=>$daystamp,
+		 'lat'=>$lat,
+		 'long'=>$long,
+		 'zone'=>$zone
+		);
 	}
 
 	//Get no. in {0.0..24.0} representing the actual or notional slot-length
+	//to assist interpretation of ambiguous hour-of-day or day-of-month values
 	private function _GetSlotHours(&$bdata)
 	{
 		if($bdata['placegap'])
@@ -74,13 +64,119 @@ class tmtCalendar extends Calendar
 		return 0.0;
 	}
 
+	private function _timecheck(&$times,$sameday)
+	{
+		foreach($times as &$range)
+		{
+			//TODO interpet any sun-related times
+			$s = ($sameday) ? MAX($range[0],$tstart) : $range[0];
+			//TODO support roll-over to contiguous day(s) & time(s)
+			if($range[1] >= $s+$length)
+			{
+				unset($range);
+				return $s;
+			}
+		}
+		unset($range);
+		return FALSE;
+	}
+
+	/**
+	MonthNames:
+
+	Get one, or array of, translated month-name(s)
+
+	@which: 1 (for January) .. 12 (for December), or array of such indices
+	@short: optional, whether to get short-form name, default FALSE
+	*/
+	function MonthNames($which,$short=FALSE)
+	{
+		$k = ($short) ? 'shortmonths' : 'longmonths';
+		$all = explode(',',$this->mod->Lang($k));
+
+		if (!is_array($which))
+		{
+			if ($which > 0 && $which < 13)
+				return $all[$which-1];
+			return '';
+		}
+		$ret = array();
+		foreach ($which as $month)
+		{
+			if ($month > 0 && $month < 13)
+				$ret[$month] = $all[$month-1];
+		}
+		return $ret;
+	}
+
+	/**
+	DayNames:
+
+	Get one, or array of, translated day-name(s)
+
+	@which: 1 (for Sunday) .. 7 (for Saturday), or array of such indices
+	@short: optional, whether to get short-form name, default FALSE
+	*/
+	function DayNames($which,$short=FALSE)
+	{
+		$k = ($short) ? 'shortdays' : 'longdays';
+		$all = explode(',',$this->mod->Lang($k));
+
+		if (!is_array($which))
+		{
+			if ($which > 0 && $which < 8)
+				return $all[$which-1];
+			return '';
+		}
+		$ret = array();
+		foreach ($which as $day)
+		{
+			if ($day > 0 && $day < 8)
+				$ret[$day] = $all[$day-1];
+		}
+		return $ret;
+	}
+
+	/**
+	IntervalNames:
+
+	Get one, or array of, translated time-interval-name(s)
+
+	@which: index 0 (for 'none'), 1 (for 'minute') .. 6 (for 'year'), or array of such indices
+	@plural: optional, whether to get plural form of the interval name(s), default FALSE
+	@cap: optional, whether to capitalise the first character of the name(s), default FALSE
+	*/
+	function IntervalNames($which,$plural=FALSE,$cap=FALSE)
+	{
+		$k = ($plural) ? 'multiperiods' : 'periods';
+		$all = explode(',',$this->mod->Lang($k));
+		array_unshift($all,$this->mod->Lang('none'));
+
+		if (!is_array($which))
+		{
+			if ($which >= 0 && $which < 7)
+				return $all[$which];
+			return '';
+		}
+		$ret = array();
+		foreach($which as $period)
+		{
+			if ($period >= 0 && $period < 7)
+			{
+				$ret[$period] = ($cap) ? ucfirst($all[$period]): //for current locale
+					$all[$period];
+			}
+		}
+		return $ret;
+	}
+
 	/**
 	SlotComplies:
 
 	Determine whether the interval @start to @start + @length satisfies constraints
 	specified in relevant fields in @bdata. Also returns FALSE if the
 	availability-descriptor string is malformed.
-	ParseCondition() must be called before this func.
+	parent::CheckCondition() or ::ParseCondition() must be called before this func.
 
 	@bdata: reference to array of data for current bracket 	
 	@start: preferred start time (bracket-local timestamp)
@@ -91,13 +187,16 @@ class tmtCalendar extends Calendar
 	{
 		if($this->conds == FALSE)
 			return FALSE;
-		$sunstuff = $this->_GetSunData($bdata,$start);
-		$maxhours = $this->_GetSlotHours($bdata);
-		foreach ($this->conds as $cond)
+		$sunstuff = self::_GetSunData($bdata,$start);
+		$maxhours = self::_GetSlotHours($bdata);
+		$dstart = floor($start/86400);
+		$dend = $dstart + $laterdays;
+		$tstart = $start - $dstart;
+		foreach($this->conds as &$cond)
 		{
-			if(0) //TODO
-				return TRUE;
+			//TODO
 		}
+		unset($cond);
 		return FALSE;
 	}
 
@@ -107,9 +206,9 @@ class tmtCalendar extends Calendar
 	Get start-time (timestamp) matching constraints specified in relevant fields in
 	@bdata, and	starting no sooner than @start, or ASAP within @later days after
 	the one including @start, and where the available time is at least @length.
-	Returns FALSE if no such time is available within the specified interval, or
-	the availability-descriptor string is malformed.
-	ParseCondition() must be called before this func.
+	Returns FALSE if no such time is available within the specified interval (or
+	the availability-descriptor string is malformed).
+	parent::CheckCondition() or ::ParseCondition() must be called before this func.
 
 	@bdata: reference to array of data for current bracket
 	@start: preferred/first start time (bracket-local timestamp)
@@ -120,13 +219,51 @@ class tmtCalendar extends Calendar
 	{
 		if($this->conds == FALSE)
 			return FALSE;
-		$sunstuff = $this->_GetSunData($bdata,$start);
-		$maxhours = $this->_GetSlotHours($bdata);
-		foreach ($this->conds as $cond)
+		$sunstuff = self::_GetSunData($bdata,$start);
+		$maxhours = self::_GetSlotHours($bdata);
+		$dstart = floor($start/86400);
+		$dend = $dstart + $laterdays;
+		$tstart = $start - $dstart;
+		foreach($this->conds as &$cond)
 		{
-			if(0) //TODO
-				return TRUE;
+			$times = $cond[2];
+			if(!$times)
+				$times = array(0=>array(0,86399)); //whole day's worth of seconds
+			if($cond[1] == FALSE) //time(s) on any day
+			{
+				$X = self::_timecheck($times,TRUE);
+				if($X !== FALSE)
+				{
+					uset($cond);
+					return $X; //TODO + $cond[1]>day-index * 86400 + zone offset seconds
+				}
+				if($laterdays > 0)
+				{
+					$X = self::_timecheck($times,FALSE);
+					if($X !== FALSE)
+					{
+						uset($cond);
+						return $X; //TODO + $cond[1]>day-index * 86400 + zone offset seconds
+					}
+				}
+			}
+			else
+			{
+				/*interpret $cond[1]
+				  foreach day of interpreted
+				    get day-index
+						if IN $dstart to $dend inclusive
+							$sameday = (day-index == $dstart);
+							$X = self::_timecheck($times,$sameday);
+							if($X !== FALSE)
+							{
+								uset($cond);
+								return $X; //TODO + $cond[1]>day-index * 86400 + zone offset seconds
+							}
+				*/
+			}
 		}
+		unset($cond);
 		return FALSE;
 	}
 
