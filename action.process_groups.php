c6fd8fc3180d3f59111f508b767159907c2a5eab
tom2tom <tpgww@onepost.net>
2015-03-09 21:44:17 +1100

    group-actions

---
+++ action.process_groups.php
@@ -0,0 +1,62 @@
+<?php
+/*
+This file is part of CMS Made Simple module: Tourney.
+Copyright (C) 2014-2015 Tom Phane <tpgww@onepost.net>
+Refer to licence and other details at the top of file Tourney.module.php
+More info at http://dev.cmsmadesimple.org/projects/tourney
+ 
+Processes form-submission from admin groups tab
+*/
+
+if(isset($params['cancel']) || empty($params['selgroups']))
+	$this->Redirect($id, 'defaultadmin','',array('showtab'=>1));
+
+$vals = array_flip($params['selgroups']); //convert strings
+$vals = array_flip($vals);
+
+if(isset($params['update']))
+{
+	$pref = cms_db_prefix();
+	$sql = 'UPDATE '.$pref.'module_tmt_groups SET name=?,flags=? WHERE group_id=?';
+	foreach($vals as $indx=>$thisid)
+	{
+		$name = $params['group_names'][$indx];
+		if($thisid)
+			$active = (int)$params['activegroups'][$indx]; //TODO mask lowest bit
+		else
+			$active = 1; //default/ungrouped always active
+		$db->Execute($sql,array($name,$active,(int)$thisid));
+	}
+}
+elseif(isset($params['delete']))
+{
+	$vals = array_diff($vals,array(0,'0','')); //preserve default
+	$vc = count($vals);
+	if($vc)
+	{
+		$fillers = str_repeat('?,',$vc-1).'?';
+		$pref = cms_db_prefix();
+		$sql = 'UPDATE '.$pref.'module_tmt_brackets SET groupid = 0 WHERE groupid IN ('.$fillers.')';
+		$db->Execute($sql,$vals);
+		$sql = 'DELETE FROM'.$pref.'module_tmt_groups WHERE group_id IN ('.$fillers.')';
+		$db->Execute($sql,$vals);
+	}
+}
+elseif(isset($params['activate']))
+{
+	$vals = array_diff($vals,array(0,'0','')); //preserve default
+	$vc = count($vals);
+	if($vc)
+	{
+		$fillers = str_repeat('?,',$vc-1).'?';
+		$pref = cms_db_prefix();
+		$state = FALSE; //TODO $state = func(VALUES OF $params['activegroups']);
+		$sql = 'UPDATE '.$pref.'module_tmt_groups SET flags=? WHERE groupid IN ('.$fillers.')';
+		array_unshift($vals,$state);
+		$ares = $db->Execute($sql,$vals);
+	}
+}
+
+$this->Redirect($id, 'defaultadmin','',array('showtab'=>1));
+
+?>
---
+++ action.swap_groups.php
@@ -0,0 +1,39 @@
+<?php
+/*
+This file is part of CMS Made Simple module: Tourney.
+Copyright (C) 2014-2015 Tom Phane <tpgww@onepost.net>
+Refer to licence and other details at the top of file Tourney.module.php
+More info at http://dev.cmsmadesimple.org/projects/tourney
+*/
+
+if (!isset($params['category_id']))
+	$this->Redirect($id, 'defaultadmin', '', array('showtab'=>1, 'tmt_message'=>$this->PrettyMessage('err_value',FALSE)));
+
+$pref = ;
+$sql = 'SELECT vieworder FROM '.$pref.'module_tmt_groups WHERE group_id=?';
+$thiscat = array($params['category_id']);
+$num1 = $db->GetOne($sql,$thiscat);
+if ($num1 === FALSE)
+	$this->Redirect($id, 'defaultadmin', '', array('showtab'=>1, 'tmt_message'=>$this->PrettyMessage('err_value',FALSE)));
+
+$othercat = array();
+if (isset($params['next_id']))
+	$othercat[] = $params['next_id'];
+elseif (isset($params['prev_id']))
+	$othercat[] = $params['prev_id'];
+else
+	$this->Redirect($id, 'defaultadmin', '', array('showtab'=>1));
+
+$num2 = $db->GetOne($sql,$othercat);
+if ($num2 === FALSE)
+	$this->Redirect($id, 'defaultadmin', '', array('showtab'=>1, 'tmt_message'=>$this->PrettyMessage('err_value',FALSE)));
+
+$sql = 'UPDATE '.$pref.'module_tmt_groups SET vieworder=? WHERE group_id=?';
+array_unshift($thiscat,$num2);
+$db->Execute($sql,$thiscat);
+array_unshift($othercat,$num1);
+$db->Execute($sql,$othercat);
+
+$this->Redirect($id, 'defaultadmin','',array('showtab'=>1));
+
+?>
