e07c384ddc99233b410a518ece4e88e3a0c6a3b5
tom2tom <tpgww@onepost.net>
2015-03-11 14:54:29 +1100

    export code to class

--- action.clone_comp.php
+++ action.clone_comp.php
@@ -10,106 +10,13 @@ if (! $this->CheckAccess('admod'))
 	$this->Redirect($id,'defaultadmin','',
 		array('tmt_message'=>$this->PrettyMessage('lackpermission',FALSE)));
 
-$sid = $params['bracket_id'];
-$pref = cms_db_prefix();
-$sql = 'SELECT * FROM '.$pref.'module_tmt_brackets WHERE bracket_id=?';
-$bdata = $db->GetRow($sql,array($sid));
-if ($bdata == FALSE)
+$bid = (int)$params['bracket_id'];
+$funcs = new tmtClone();
+$res = $funcs->CloneBracket($this,$bid);
+if($res === TRUE)
+	$this->Redirect($id,'addedit_comp',$returnid,array('bracket_id'=>$bid));
+else
 	$this->Redirect($id,'defaultadmin','',
-		array('tmt_message'=>$this->PrettyMessage('err_missing',FALSE)));
-$bid = $db->GenID($pref.'module_tmt_brackets_seq');
-$bdata['bracket_id'] = $bid;
-$c = $this->Lang('clone');
-$bdata['name'] .= ' '.$c;
-if($bdata['alias'])
-	$bdata['alias'] .= '_'.strtolower($c);
-//check for date(s) > local current date
-$tz = new DateTimeZone($bdata['timezone']);
-$dt = new DateTime ('now',$tz);
-$stamp = $dt->getTimestamp();
-$sdt = new DateTime ($bdata['startdate'],$tz);
-$sstamp = $sdt->getTimestamp();
-if ($stamp >= $sstamp)
-	$bdata['startdate'] = null;
-$sdt = new DateTime ($bdata['enddate'],$tz);
-$sstamp = $sdt->getTimestamp();
-if ($stamp >= $sstamp)
-	$bdata['enddate'] = null;
-if($bdata['chartcss'])
-{
-	$updir = $this->GetPreference('uploads_dir');
-	if ($updir)
-		$csspath = cms_join_path($config['uploads_path'],$updir,$bdata['chartcss']);
-	else
-		$csspath = cms_join_path($config['uploads_path'],$bdata['chartcss']);
-	if(!file_exists($csspath))
-		$bdata['chartcss'] = '';
-}
-$bdata['chartbuild'] = 1;
-
-$values = array_values($bdata);
-$fc = count($values);
-$fillers = str_repeat('?,',$fc-1);
-$sql = 'INSERT INTO '.$pref."module_tmt_brackets VALUES ($fillers?)";
-$db->Execute($sql,$values);
-
-$tpl = $this->GetTemplate('mailout_'.$sid.'_template');
-if ($tpl)
-	$this->SetTemplate('mailout_'.$bid.'_template',$tpl);
-$tpl = $this->GetTemplate('mailin_'.$sid.'_template');
-if ($tpl)
-	$this->SetTemplate('mailin_'.$bid.'_template',$tpl);
-$tpl = $this->GetTemplate('tweetout_'.$sid.'_template');
-if ($tpl)
-	$this->SetTemplate('tweetout_'.$bid.'_template',$tpl);
-$tpl = $this->GetTemplate('tweetin_'.$sid.'_template');
-if ($tpl)
-	$this->SetTemplate('tweetin_'.$bid.'_template',$tpl);
-$tpl = $this->GetTemplate('chart_'.$sid.'_template');
-if ($tpl)
-	$this->SetTemplate('chart_'.$bid.'_template',$tpl);
-
-/* include this if teams are to be copied too
-$sql = 'SELECT * FROM '.$pref.'module_tmt_teams WHERE bracket_id=? AND flags!=2';
-$teams = $db->GetAll($sql,array($sid));
-if ($teams)
-{
-	$swaps = array();
-	$fc = count($teams[0]);
-	$fillers = str_repeat('?,',$fc-1);
-	$sql = 'INSERT INTO '.$pref."module_tmt_teams VALUES ($fillers?)";
-	foreach($teams as &$thisteam)
-	{
-		$tid = $db->GenID($pref.'module_tmt_teams_seq');
-		$swaps[] = array($thisteam['team_id'],$tid);
-		$thisteam['team_id'] = $tid;
-		$thisteam['bracket_id'] = $bid;
-		$thisteam['flags'] = 0;
-		$values = array_values($thisteam);
-		$db->Execute($sql,$values);
-	}
-	unset($thisteam);
-	$sql = 'SELECT * FROM '.$pref.'module_tmt_people WHERE id=? AND flags!=2';
-	$sql2 = 'INSERT INTO '.$pref.'module_tmt_people VALUES (?,?,?,?,?)';
-	foreach($swaps as &$thisteam)
-	{
-		$people = $db->GetAll($sql,array($thisteam[0]));
-		if ($people)
-		{
-			foreach ($people as &$person)
-			{
-				$person['id'] = $thisteam[1];
-				$person['flags'] = 0;
-				$db->Execute($sql2,array_values($person));
-			}
-			unset($person);
-		}
-	}
-	unset($thisteam);
-	//no matches are copied
-}
-*/
-
-$this->Redirect($id,'addedit_comp',$returnid,array('bracket_id'=>$bid));
+		array('tmt_message'=>$this->PrettyMessage($res,FALSE)));
 
 ?>
--- action.delete_comp.php
+++ action.delete_comp.php
@@ -6,53 +6,17 @@ Refer to licence and other details at the top of file Tourney.module.php
 More info at http://dev.cmsmadesimple.org/projects/tourney
 */
 
-if (! $this->CheckAccess('admod')) exit;
+if (!$this->CheckAccess('admod'))
+	$this->Redirect($id,'defaultadmin','',
+		array('tmt_message'=>$this->PrettyMessage('lackpermission',FALSE)));
+
 if (empty($params['bracket_id']))
-	$this->Redirect($id, 'defaultadmin');
+	$this->Redirect($id,'defaultadmin');
 
-$bid = (int)$params['bracket_id'];
-$pref = cms_db_prefix();
-$sql = 'SELECT chartcss FROM '.$pref.'module_tmt_brackets WHERE bracket_id=?';
-$file = $db->GetOne($sql,array($bid));
-if ($file)
-{
-	$sql = 'SELECT COUNT(*) AS sharers FROM '.$pref.'module_tmt_brackets WHERE chartcss=?';
-	$num = $db->GetOne($sql,array($file));
-	if ($num < 2)
-	{
-		if($this->GetPreference('uploads_dir'))
-			$path = cms_join_path($config['uploads_path'],
-				$this->GetPreference('uploads_dir'),$file);
-		else
-			$path = cms_join_path($config['uploads_path'],$file);
-		if(is_file($path))
-			unlink($path);
-	}
-}
-$file = $this->ChartImageFile($bid);
-if ($file)
-	unlink($file);
+$funcs = new tmtDelete();
+$funcs->DeleteBracket($this,(int)$params['bracket_id']);
 
-$sql = 'DELETE FROM '.$pref.'module_tmt_tweet WHERE bracket_id=?';
-$db->Execute($sql,array($bid));
-$sql = 'DELETE FROM '.$pref.'module_tmt_people WHERE id IN
- (SELECT team_id FROM '.$pref.'module_tmt_teams WHERE bracket_id=?)';
-$db->Execute($sql,array($bid));
-$sql = 'DELETE FROM '.$pref.'module_tmt_teams WHERE bracket_id=?';
-$db->Execute($sql,array($bid));
-$sql = 'DELETE FROM '.$pref.'module_tmt_matches WHERE bracket_id=?';
-$db->Execute($sql,array($bid));
-$sql = 'DELETE FROM '.$pref.'module_tmt_brackets WHERE bracket_id=?';
-$db->Execute($sql,array($bid));
-
-$bid = $params['bracket_id'];
-$this->DeleteTemplate('mailout_'.$bid.'_template');
-$this->DeleteTemplate('mailin_'.$bid.'_template');
-$this->DeleteTemplate('tweetout_'.$bid.'_template');
-$this->DeleteTemplate('tweetin_'.$bid.'_template');
-$this->DeleteTemplate('chart_'.$bid.'_template');
-
-unset($params);
-$this->Redirect($id, 'defaultadmin', '', array('tmt_message'=>$this->PrettyMessage('comp_deleted')));
+$this->Redirect($id,'defaultadmin','',
+	array('tmt_message'=>$this->PrettyMessage('comp_deleted')));
 
 ?>
