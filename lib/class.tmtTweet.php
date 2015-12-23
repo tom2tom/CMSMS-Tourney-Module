0564c080313a0a3ed667c132295f82c37abfaa4b
tom2tom <tpgww@onepost.net>
2015-12-23 13:17:55 +1100

    adapt to work with Notifier module

--- lib/class.tmtTweet.php
+++ lib/class.tmtTweet.php
@@ -10,124 +10,25 @@ This class is not suitable for static method-calling
 */
 class tmtTweet
 {
-	private $twt;
+	private $mod; //reference to Tourney module object
+	private $twt; //reference to TweetSender object
+	private $smarty; //reference to current smarty object, with some template-vars already set
 	//details for default twitter application: CMSMS TourneyModule, owned by @CMSMSTourney
 	private $api_key = 'JnUL9AU1RxOW8xIjrBXeZfTnr';
-	private $api_secret = 'MUVQMUJ6ZGZWMGc1TGhYaU9Xbkk0a2tXakJiOThFU2hzOFFRMW5Ca3JYVFJrWUVERVU='; 
+	private $api_secret = 'MUVQMUJ6ZGZWMGc1TGhYaU9Xbkk0a2tXakJiOThFU2hzOFFRMW5Ca3JYVFJrWUVERVU=';
 	private $access_token = '2426259434-64ADfkcEKgyUr1BL63HIPLOdCbgmkM6Zjdt55tp';
 	private $access_secret = 'bEF5R3BqQzBOQ2lUanV3QVBtQUJTOWNTSjNNWnYxWDN2UkFSeDdHVDNuSFdS';
 
-	function ModuleAppTokens()
+	function __construct(&$mod,&$twt,&$smarty)
 	{
-		return array($this->api_key,base64_decode($this->api_secret));
-	}
-
-	/**
-	GetTokens:
-	@bracket_id: bracket identifier
-	@exact: boolean, whether to revert to codes for CMSMSTourney, optional
-	@test: boolean, whether to return TRUE or array of codes, optional
-	Returns: associative array of Twitter access codes, or FALSE
-	*/
-	function GetTokens($bracket_id,$exact=FALSE,$test=FALSE)
-	{
-		$pref = cms_db_prefix();
-		$db = cmsms()->GetDB();
-		$sql = 'SELECT twtfrom FROM '.$pref.'module_tmt_brackets WHERE bracket_id=?';
-		$handle = $db->GetOne($sql,array($bracket_id));
-		if($handle)
-		{
-			if($handle[0] != '@')
-				$handle = '@'.$handle;
-			if(!self::ValidateAddress($handle))
-				return FALSE;
-			if($handle != '@CMSMSTourney')
-			{
-				$sql = 'SELECT DISTINCT bracket_id,pubtoken,privtoken FROM '.$pref.'module_tmt_tweet WHERE handle=? AND bracket_id IN (?,0)';
-				$choices = $db->GetAssoc($sql,array($handle,$bracket_id));
-				if(!$choices)
-					return FALSE;
-				if($test)
-					return TRUE;
-				if(!isset($choices[$bracket_id]))
-					$bracket_id = 0;
-				$apub = $choices[$bracket_id]['pubtoken'];
-				$apriv = base64_decode($choices[$bracket_id]['privtoken']);
-			}
-			else
-			{
-				if($test)
-					return TRUE;
-				$apub = $this->access_token;
-				$apriv = base64_decode($this->access_secret);
-			}
-		}
-		elseif(!$exact)
-		{
-			if($test)
-				return TRUE;
-			$apub = $this->access_token;
-			$apriv = base64_decode($this->access_secret);
-		}
-		else
-			return FALSE;
-
-		$pub = $this->api_key;
-		$priv = base64_decode($this->api_secret);
-
-		return array ('api_key'=>$pub,'api_secret'=>$priv,
-			'access_token'=>$apub,'access_secret'=>$apriv);
-	}
-
-	/**
-	SaveTokens:
-	Save Twitter keys.
-	At least one of $handle or @bracket_id must be provided
-	@pub: public-key to be saved
-	@priv: private-key to be saved
-	@handle: twitter handle (maybe without leading '@'), optional
-	@bracket_id: bracket identifier, optional
-	Returns: boolean - whether the save succeeded
-	*/
-	function SaveTokens($pub,$priv,$handle=FALSE,$bracket_id=0)
-	{
-		if(!($handle || $bracket_id))
-			return FALSE;
-		$pref = cms_db_prefix();
-		$db = cmsms()->GetDB();
-		if(!$handle)
-		{
-			$sql = 'SELECT twtfrom FROM '.$pref.'module_tmt_brackets WHERE bracket_id=?';
-			$handle = $db->GetOne($sql,array($bracket_id));
-			if(!$handle)
-				return FALSE;
-		}
-		if($handle[0] != '@')
-			$handle = '@'.$handle;
-		if(!self::ValidateAddress($handle))
-			return FALSE;
-		$priv = base64_encode($priv); //no point in more sophistication e.g. encryption
-		$sql = 'UPDATE '.$pref.'module_tmt_tweet SET pubtoken=?,privtoken=? WHERE bracket_id=? AND handle=?';
-		$sql2 = 'INSERT INTO '.$pref.'module_tmt_tweet (bracket_id,handle,pubtoken,privtoken)
-SELECT DISTINCT ?,?,?,? FROM '.$pref.'module_tmt_tweet WHERE NOT EXISTS
-(SELECT 1 FROM '.$pref.'module_tmt_tweet WHERE bracket_id=? AND handle=?)';
-		//minimal race-chance here
-		$db->StartTrans();
-		$db->Execute($sql,array($pub,$priv,$bracket_id,$handle));
-		$db->Execute($sql2,array($bracket_id,$handle,$pub,$priv,$bracket_id,$handle));
-		if($bracket_id)
-		{
-			//ensure sender is current
-			$sql = 'UPDATE '.$pref.'module_tmt_brackets SET twtfrom=? WHERE bracket_id=?';
-			$db->Execute($sql,array($handle,$bracket_id));
-		}
-		return $db->CompleteTrans();
+		$this->mod = $mod;
+		$this->twt = $twt;
+		$this->smarty = $smarty;
 	}
 
 	/**
 	DoSend:
 	Sends tweet(s) about a match
-	@mod: reference to current module object
 	@codes: associative array of 4 Twitter access-codes
 	@to: array of validated hashtag(s) for recipient(s)
 	@tpl: smarty template to use for message body
@@ -135,40 +36,21 @@ SELECT DISTINCT ?,?,?,? FROM '.$pref.'module_tmt_tweet WHERE NOT EXISTS
 	 [0] FALSE if no addressee or no twitter module, otherwise boolean cumulative result of twt->Send()
 	 [1] '' or error message e.g. from twt->send()
 	*/
-	private function DoSend(&$mod,$codes,$to,$tpl)
+	private function DoSend($codes,$to,$tpl)
 	{
 		if(!$to)
 			return array(FALSE,'');
 		if(!$this->twt)
-		{
-			$this->twt = new TTwitter($codes['api_key'],$codes['api_secret'],
-				$codes['access_token'],$codes['access_secret']);
-			if(!$this->twt)
-				return array(FALSE,$mod->Lang('err_system'));
-		 }
+			return array(FALSE,$this->mod->Lang('err_system'));
+		$creds = array(
+			'api_key'=>$this->api_key,
+			'api_secret'=>base64_decode($this->api_secret),
+			'access_token'=>$this->access_token,
+			'access_secret'=>base64_decode($this->access_secret)
+		);
+		$body = $this->mod->ProcessDataTemplate($tpl);
 
-		$err = '';
-		$body = $mod->ProcessDataTemplate($tpl);
-		$lb = strlen($body);
-		
-		foreach($to as $hash)
-		{
-			$lh = strlen($hash);
-			if($lb+$lh <= 139)
-				$main = $body;
-			else
-				$main = substr($body,0,139-$lh);
-			try
-			{
-				$this->twt->send($main.' '.$hash);
-			}
-			catch (TwitterException $e)
-			{
-				if($err) $err .= '<br />';
-				$err .= $hash.': '.$e->getMessage();
-			}
-		}
-		return array(($err==''),$err);
+		return $this->twt->Send($creds,FALSE,$to,$body);
 	}
 
 	/**
@@ -195,7 +77,7 @@ SELECT DISTINCT ?,?,?,? FROM '.$pref.'module_tmt_tweet WHERE NOT EXISTS
 			$sends = array();
 			foreach($members as $one)
 			{
-				$clean = self::ValidateAddress($one['contact']);
+				$clean = $this->twt->ValidateAddress($one['contact']);
 				if($clean)
 				{
 					$clean[0] = '#';
@@ -205,37 +87,6 @@ SELECT DISTINCT ?,?,?,? FROM '.$pref.'module_tmt_tweet WHERE NOT EXISTS
 				}
 			}
 			return $sends;
-		}
-		return FALSE;
-	}
-
-	/**
-	ValidateAddress:
-	Check whether @address is a valid twitter handle
-	@address: one, or a comma-separated series of, address(es) to check
-	Returns: a trimmed valid twitter handle, or array of them, or FALSE
-	*/
-	public function ValidateAddress($address)
-	{
-		$pattern = '/^@\w{1,15}$/';
-		if(strpos($address,',') === FALSE)
-		{
-			$address = trim($address);
-			if(preg_match($pattern,$address))
-				return $address;
-		}
-		else
-		{
-			$parts = explode(',',$address);
-			$address = array();
-			foreach($parts as $one)
-			{
-				$one = trim($one);
-				if(preg_match($pattern,$one))
-					$address[] = $one;
-			}
-			if($address)
-				return $address;
 		}
 		return FALSE;
 	}
@@ -252,7 +103,7 @@ SELECT DISTINCT ?,?,?,? FROM '.$pref.'module_tmt_tweet WHERE NOT EXISTS
 		$pref = cms_db_prefix();
 		$sql = 'SELECT twtfrom FROM '.$pref.'tmt_module_brackets WHERE bracket_id=?';
 		$source = $db->GetOne($sql,array($bracket_id));
-		if(!$source || !self::ValidateAddress($source))
+		if(!$source || !$this->twt->ValidateAddress($source))
 			return FALSE;
 		$sql = 'SELECT teamA,teamB FROM '.$pref.'tmt_module_matches WHERE match_id';
 		if(!is_array($mid))
@@ -277,9 +128,7 @@ SELECT DISTINCT ?,?,?,? FROM '.$pref.'module_tmt_tweet WHERE NOT EXISTS
 
 	/**
 	TellOwner:
-	Sends tweet to tournament owner if possible, and then to one member of both teams in the match
-	@mod: reference to current module object
-	@smarty: reference to CMSMS smarty object
+	Sends tweets with respective hashtags of tournament owner, and one member of both teams in the match
 	@bdata: reference to array of bracket-table data
 	@mdata: reference to array of match data (from which we need 'teamA', 'teamB')
 	@lines: array of lines for message body
@@ -287,10 +136,10 @@ SELECT DISTINCT ?,?,?,? FROM '.$pref.'module_tmt_tweet WHERE NOT EXISTS
 	 [0] TRUE|FALSE representing success
 	 [1] '' or specific failure message
 	*/
-	public function TellOwner(&$mod,&$smarty,&$bdata,&$mdata,$lines)
+	public function TellOwner(&$bdata,&$mdata,$lines)
 	{
 		//owner
-		$clean = self::ValidateAddress($bdata['contact']);
+		$clean = $this->twt->ValidateAddress($bdata['contact']);
 		if($clean)
 		{
 			$clean[0] = '#';
@@ -300,7 +149,7 @@ SELECT DISTINCT ?,?,?,? FROM '.$pref.'module_tmt_tweet WHERE NOT EXISTS
 			return array(FALSE,''); //silent, try another channel
 		$tokens = self::GetTokens($bdata['bracket_id']);
 		if(!$tokens)
-			return array(FALSE,$mod->Lang('lackpermission'));
+			return array(FALSE,$this->mod->Lang('lackpermission'));
 		//teams
 		$tid = (int)$mdata['teamA'];
 		if($tid > 0)
@@ -317,19 +166,17 @@ SELECT DISTINCT ?,?,?,? FROM '.$pref.'module_tmt_tweet WHERE NOT EXISTS
 				$to = array_merge($to,$more);
 		}
 		//submitted data
-		$smarty->assign('report',implode(' ',$lines));
+		$this->smarty->assign('report',implode(' ',$lines));
 
-		$tpl = $mod->GetTemplate('tweetin_'.$bdata['bracket_id'].'_template');
+		$tpl = $this->mod->GetTemplate('tweetin_'.$bdata['bracket_id'].'_template');
 		if($tpl == FALSE)
-			$tpl = $mod->GetTemplate('tweetin_default_template');
-		return self::DoSend($mod,$tokens,$to,$tpl);
+			$tpl = $this->mod->GetTemplate('tweetin_default_template');
+		return self::DoSend($this->mod,$tokens,$to,$tpl);
 	}
-	
+
 	/**
 	TellTeams:
-	Sends tweet to one or all members of both teams in the match, plus the owner
-	@mod: reference to current module object
-	@smarty: reference to CMSMS smarty object
+	Sends tweets with respective hashtags of one or all members of both teams in the match, plus the owner
 	@bdata: reference to array of bracket-table data
 	@mdata: reference to array of match data (from which we need 'teamA', 'teamB')
 	@tpl: enum for type of message: 1 = announcement, 2 = cancellation, 3 = score-request
@@ -339,28 +186,28 @@ SELECT DISTINCT ?,?,?,? FROM '.$pref.'module_tmt_tweet WHERE NOT EXISTS
 	 [0] TRUE|FALSE representing success, or TRUE if nobody to send to
 	 [1] '' or specific failure message
 	*/
-	public function TellTeams(&$mod,&$smarty,&$bdata,&$mdata,$tpl,$first=FALSE)
+	public function TellTeams(&$bdata,&$mdata,$tpl,$first=FALSE)
 	{
 		switch($tpl)
 		{
 		 case 1:
-			$tpl = $mod->GetTemplate('tweetout_'.$bdata['bracket_id'].'_template');
+			$tpl = $this->mod->GetTemplate('tweetout_'.$bdata['bracket_id'].'_template');
 			if($tpl == FALSE)
-				$tpl = $mod->GetTemplate('tweetout_default_template');
+				$tpl = $this->mod->GetTemplate('tweetout_default_template');
 			break;
 		 case 2:
-			$tpl = $mod->GetTemplate('tweetcancel_'.$bdata['bracket_id'].'_template');
+			$tpl = $this->mod->GetTemplate('tweetcancel_'.$bdata['bracket_id'].'_template');
 			if($tpl == FALSE)
-				$tpl = $mod->GetTemplate('tweetcancel_default_template');
+				$tpl = $this->mod->GetTemplate('tweetcancel_default_template');
 			break;
 		 case 3:
-			$tpl = $mod->GetTemplate('tweetrequest_'.$bdata['bracket_id'].'_template');
+			$tpl = $this->mod->GetTemplate('tweetrequest_'.$bdata['bracket_id'].'_template');
 			if($tpl == FALSE)
-				$tpl = $mod->GetTemplate('tweetrequest_default_template');
+				$tpl = $this->mod->GetTemplate('tweetrequest_default_template');
 			break;
 		}
 
-		$owner = self::ValidateAddress($bdata['contact']);
+		$owner = $this->twt->ValidateAddress($bdata['contact']);
 		if($owner)
 			$owner[0] = '#';
 		$tokens = self::GetTokens($bdata['bracket_id']);
@@ -375,39 +222,39 @@ SELECT DISTINCT ?,?,?,? FROM '.$pref.'module_tmt_tweet WHERE NOT EXISTS
 				if($tokens)
 				{
 					reset($to);
-					$smarty->assign('recipient',key($to));
+					$this->smarty->assign('recipient',key($to));
 					$tc = count($to);
 					$toall = (($bdata['teamsize'] < 2 && $tc > 0) || $tc > 1);
-					$smarty->assign('toall',$toall);
+					$this->smarty->assign('toall',$toall);
 					if ((int)$mdata['teamB'] > 0)
-						$op = $mod->TeamName($mdata['teamB']);
+						$op = $this->mod->TeamName($mdata['teamB']);
 					else
 					{
 /*					switch($bdata['type'])
 						{
 						 case Tourney::KOTYPE:
-							$op = $mod->Lang('anonwinner');
+							$op = $this->mod->Lang('anonwinner');
 							break;
 						 default:
-							$op = $mod->Lang('anonother');
+							$op = $this->mod->Lang('anonother');
 							break;
 						}
 */
 						$op = '';
 					}
-					$smarty->assign('opponent',$op);
+					$this->smarty->assign('opponent',$op);
 					if($owner)
 						$to[] = $owner;
-					list($resA,$msg) = self::DoSend($mod,$tokens,$to,$tpl);
+					list($resA,$msg) = self::DoSend($this->mod,$tokens,$to,$tpl);
 					if(!$resA)
 					{
 						if(!$msg)
-							$msg = $mod->Lang('err_notice');
-						$err .= $mod->TeamName($tid).': '.$msg;
+							$msg = $this->mod->Lang('err_notice');
+						$err .= $this->mod->TeamName($tid).': '.$msg;
 					}
 				}
 				else
-					$err = $mod->TeamName($tid).': '.$mod->Lang('lackpermission');
+					$err = $this->mod->TeamName($tid).': '.$this->mod->Lang('lackpermission');
 			}
 		}
 
@@ -421,42 +268,42 @@ SELECT DISTINCT ?,?,?,? FROM '.$pref.'module_tmt_tweet WHERE NOT EXISTS
 				if($tokens)
 				{
 					reset($to);
-					$smarty->assign('recipient',key($to));
+					$this->smarty->assign('recipient',key($to));
 					$tc = count($to);
 					$toall = (($bdata['teamsize'] < 2 && $tc > 0) || $tc > 1);
-					$smarty->assign('toall',$toall);
+					$this->smarty->assign('toall',$toall);
 					if ((int)$mdata['teamA'] > 0)
-						$op = $mod->TeamName($mdata['teamA']);
+						$op = $this->mod->TeamName($mdata['teamA']);
 					else
 					{
 /*					switch($bdata['type'])
 						{
 						 case Tourney::KOTYPE:
-							$op = $mod->Lang('anonwinner');
+							$op = $this->mod->Lang('anonwinner');
 							break;
 						 default:
-							$op = $mod->Lang('anonother');
+							$op = $this->mod->Lang('anonother');
 							break;
 						}
 */
 						$op = '';
 					}
-					$smarty->assign('opponent',$op);
+					$this->smarty->assign('opponent',$op);
 					if($owner)
 						$to[] = $owner;
-					list($resB,$msg) = self::DoSend($mod,$tokens,$to,$tpl);
+					list($resB,$msg) = self::DoSend($this->mod,$tokens,$to,$tpl);
 					if(!$resB)
 					{
-						if($err) $err .= '<br />'; 
+						if($err) $err .= '<br />';
 						if(!$msg)
-							$msg = $mod->Lang('err_notice');
-						$err .= $mod->TeamName($tid).': '.$msg;
+							$msg = $this->mod->Lang('err_notice');
+						$err .= $this->mod->TeamName($tid).': '.$msg;
 					}
 				}
 				else
 				{
 					if($err) $err .= '<br />';
-					$err .= $mod->TeamName($tid).': '.$mod->Lang('lackpermission');
+					$err .= $this->mod->TeamName($tid).': '.$this->mod->Lang('lackpermission');
 				}
 			}
 		}
