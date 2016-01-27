b175e157a99fa73f82e906122769cabec5d2f4f1
tom2tom <tpgww@onepost.net>
2016-01-28 09:45:31 +1100

    support availability-help

--- include/jquery.tmtfuncs.js
+++ include/jquery.tmtfuncs.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
  $('p.help').hide();
- $('img.tipper').css({'display':'inline','padding-left':'10px'}).click(function(){
-  $(this).parent().next().next().slideToggle();
- });
+ $('img.tipper').css('display','inline');
  var elem = $('p.pageinput:first');
  var color = $(elem).css('color');
  var size = $(elem).css('font-size');
  $('.fakeicon').css({'color':color,'font-size':size});
+ $('p img.tipper').click(function(){
+  $(this).parent().nextAll('p.help:first').slideToggle();
+ });
+ $('th img.tipper').click(function(){
+  $(this).closest('table').nextAll('p.help:first').slideToggle();
+ });
 });
