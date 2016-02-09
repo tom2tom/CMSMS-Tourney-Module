e52ffebf5f6b685187c15ea45bdaf28e502c0787
tom2tom <tpgww@onepost.net>
2016-02-09 12:33:12 +1100

    add requirements section to module help

--- lang/en_US.php
+++ lang/en_US.php
@@ -658,6 +658,10 @@ Truetype fonts can be used, and would typically result in smaller-sized chart fi
 </ul>
 before the account is used.</p>
 <p>For the latter, you can create, and at least temporarily enable, a page with tag like<pre>{cms_module module='Tourney' alias='sample_comp' tweetauth=1}</pre>or just<br />{cms_module module='Tourney' tweetauth=1}</pre><br /><br />and refer the account holder there.</p>
+<h3>Requirements</h3>
+<ul>
+<li>PHP 5.2+</li>
+</ul>
 <h3>Support</h3>
 <p>This module is provided as-is. Please read the text of the license for the full disclaimer.</p>
 <p>For help:<ul>
